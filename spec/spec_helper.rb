fe507b6d5540018003ea9ec3b9835581ae055e03
Alex Tomlins <alex.tomlins@unboxedconsulting.com>
2010-07-17 20:54:04 +0100

    Require rubygems in spec_herlper
    needed to run from rake

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rubygems'
 require 'action_controller'
 require 'tabnav'
 
@@ -7,5 +8,5 @@ require 'spec'
 require 'spec/autorun'
 
 Spec::Runner.configure do |config|
-  
+
 end
