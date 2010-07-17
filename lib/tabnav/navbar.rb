f88e33b9246326f2f641b92fad2acf8dd6211e27
Alex Tomlins <alex.tomlins@unboxedconsulting.com>
2010-07-17 21:17:49 +0100

    Navbar basics done.

--- lib/tabnav/navbar.rb
+++ lib/tabnav/navbar.rb
@@ -1,10 +1,26 @@
 module Tabnav
   class Navbar
-    def initialize(*args)
+    def initialize(template, options = {})
+      @template = template
+      @html_options = options
+      @tabs = []
+    end
+
+    def add_tab(options = {})
+      t = Tab.new(@template, options)
+      yield t
+      @tabs << t
     end
 
     def render
-      ''
+      return '' if @tabs.empty?
+      @template.content_tag :ul, @html_options do
+        contents = ''
+        @tabs.each do |tab|
+          contents += tab.render
+        end
+        contents
+      end
     end
   end
 end
---
+++ spec/tabnav/navbar_spec.rb
@@ -0,0 +1,109 @@
+require 'spec_helper'
+
+describe Tabnav::Navbar do
+
+  describe "add_tab" do
+    before :each do
+      @template = ActionView::Base.new()
+    end
+
+    it "should create a new tab with the passed template, and yield it to the block" do
+      n = Tabnav::Navbar.new(@template)
+      t = Tabnav::Tab.new(@template)
+      Tabnav::Tab.should_receive(:new).with(@template, {}).and_return(t)
+      n.add_tab do |t|
+        t.should == t
+      end
+    end
+
+    it "should create the tab with any passed options" do
+      n = Tabnav::Navbar.new(@template)
+      t = Tabnav::Tab.new(@template)
+      Tabnav::Tab.should_receive(:new).with(@template, {:class => "my_class", :id => "my_id"}).and_return(t)
+      n.add_tab :class => "my_class", :id => "my_id" do |t|
+        t.should == t
+      end
+    end
+  end
+
+  describe "render" do
+    before :each do
+      @template = ActionView::Base.new()
+    end
+
+    it "should output nothing if no tabs have been added" do
+      n = Tabnav::Navbar.new(@template)
+      n.render.should == ''
+    end
+
+    it "should output a ul containing the results of rendering each of it's tabs" do
+      n = Tabnav::Navbar.new(@template)
+      t1 = t2 = nil
+      n.add_tab do |t|
+        t1 = t
+      end
+      n.add_tab do |t|
+        t2 = t
+      end
+      t1.should_receive(:render).and_return("Tab 1 markup")
+      t2.should_receive(:render).and_return("Tab 2 markup")
+      n.render.should == '<ul>Tab 1 markupTab 2 markup</ul>'
+    end
+
+    it "should pass the options given on creation to the ul" do
+      n = Tabnav::Navbar.new(@template, :id => "my_id", :class => "my_class")
+      t1 = nil
+      n.add_tab do |t|
+        t1 = t
+      end
+      @template.should_receive(:content_tag).with(:ul, {:id => "my_id", :class => "my_class"}).and_return(:some_markup)
+      n.render.should == :some_markup
+    end
+
+#     it "should output a li containing a span with the name if no link given" do
+#       t = Tabnav::Tab.new(@template)
+#       t.named "A Tab"
+#       html = t.render
+#       html.should == "<li><span>A Tab</span></li>"
+#     end
+#
+#     it "should output a li containing a link with the name" do
+#       t = Tabnav::Tab.new(@template)
+#       t.named "A Tab"
+#       t.links_to "/wibble"
+#       html = t.render
+#       html.should == "<li><a href=\"/wibble\">A Tab</a></li>"
+#     end
+#
+#     context "with no name given" do
+#       it "should output a blank tab if no link given" do
+#         t = Tabnav::Tab.new(@template)
+#         html = t.render
+#         html.should == "<li><span></span></li>"
+#       end
+#
+#       it "should output an empty link" do
+#         t = Tabnav::Tab.new(@template)
+#         t.links_to "/wibble"
+#         html = t.render
+#         html.should == "<li><a href=\"/wibble\"></a></li>"
+#       end
+#     end
+#
+#     it "should pass the options given on creation to the li" do
+#       @template.should_receive(:content_tag).with(:li, {:id => "my_id", :class => "my_class"}).and_return(:some_markup)
+#       t = Tabnav::Tab.new(@template, :id => "my_id", :class => "my_class")
+#       t.named "A Tab"
+#       t.render.should == :some_markup
+#     end
+#
+#     it "should pass the options given to the link to link_to" do
+#       @template.should_receive(:link_to).with("A Tab", "/wibble", {:class => "link_class", :target => "_blank"}).and_return("A Link")
+#       t = Tabnav::Tab.new(@template)
+#       t.named "A Tab"
+#       t.links_to "/wibble", :class => "link_class", :target => "_blank"
+#       html = t.render
+#       html.should == "<li>A Link</li>"
+#     end
+  end
+end
