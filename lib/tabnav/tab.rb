2b52871dfa6337a9364b2413c172730cc4be4727
Alex Tomlins <alex.tomlins@unboxedconsulting.com>
2010-07-17 10:20:34 +0100

    Worked out rendering of Tabs.

--- lib/tabnav/tab.rb
+++ lib/tabnav/tab.rb
@@ -1,20 +1,26 @@
 module Tabnav
   class Tab
 
+    def initialize(template, html_options = {})
+      @html_options = html_options
+      @template = template
+    end
+
     def named(text)
       @text = text
     end
 
-    def links_to(url)
+    def links_to(url, link_options = {})
       @link = url
+      @link_options = link_options
     end
 
-    def render(template)
-      template.content_tag(:li) do
+    def render
+      @template.content_tag(:li, @html_options) do
         if @link
-          template.link_to @text, @link
+          @template.link_to @text, @link, @link_options
         else
-          template.content_tag :span, @text
+          @template.content_tag :span, @text
         end
       end
     end
--- spec/tabnav/tab_spec.rb
+++ spec/tabnav/tab_spec.rb
@@ -1,24 +1,40 @@
 require 'spec_helper'
 
 describe Tabnav::Tab do
-  
+
   describe "render" do
     before :each do
       @template = ActionView::Base.new()
     end
     it "should output a li containing a span with the name if no link given" do
-      t = Tabnav::Tab.new
+      t = Tabnav::Tab.new(@template)
       t.named "A Tab"
-      html = t.render(@template)
+      html = t.render
       html.should == "<li><span>A Tab</span></li>"
     end
-    
+
     it "should output a li containing a link with the name" do
-      t = Tabnav::Tab.new
+      t = Tabnav::Tab.new(@template)
       t.named "A Tab"
       t.links_to "/wibble"
-      html = t.render(@template)
+      html = t.render
       html.should == "<li><a href=\"/wibble\">A Tab</a></li>"
+    end
+
+    it "should pass the options given on creation to the li" do
+      @template.should_receive(:content_tag).with(:li, {:id => "my_id", :class => "my_class"}).and_return(:some_markup)
+      t = Tabnav::Tab.new(@template, :id => "my_id", :class => "my_class")
+      t.named "A Tab"
+      t.render.should == :some_markup
+    end
+
+    it "should pass the options given to the link to link_to" do
+      @template.should_receive(:link_to).with("A Tab", "/wibble", {:class => "link_class", :target => "_blank"}).and_return("A Link")
+      t = Tabnav::Tab.new(@template)
+      t.named "A Tab"
+      t.links_to "/wibble", :class => "link_class", :target => "_blank"
+      html = t.render
+      html.should == "<li>A Link</li>"
     end
   end
 end
