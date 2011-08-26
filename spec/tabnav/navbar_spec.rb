f17a23314e40eb1f99a9af3434eb3141d31731a8
Alex Tomlins <alex.tomlins@unboxedconsulting.com>
2011-08-26 16:06:07 +0100

    Tidy

--- spec/tabnav/navbar_spec.rb
+++ spec/tabnav/navbar_spec.rb
@@ -69,51 +69,5 @@ describe Tabnav::Navbar do
       @template.should_receive(:content_tag).with(:ul, {:id => "my_id", :class => "my_class"}).and_return(:some_markup)
       n.render_navbar.should == :some_markup
     end
-
-#     it "should output a li containing a span with the name if no link given" do
-#       t = Tabnav::Tab.new(@template)
-#       t.named "A Tab"
-#       html = t.render
-#       html.should == "<li><span>A Tab</span></li>"
-#     end
-#
-#     it "should output a li containing a link with the name" do
-#       t = Tabnav::Tab.new(@template)
-#       t.named "A Tab"
-#       t.links_to "/wibble"
-#       html = t.render
-#       html.should == "<li><a href=\"/wibble\">A Tab</a></li>"
-#     end
-#
-#     context "with no name given" do
-#       it "should output a blank tab if no link given" do
-#         t = Tabnav::Tab.new(@template)
-#         html = t.render
-#         html.should == "<li><span></span></li>"
-#       end
-#
-#       it "should output an empty link" do
-#         t = Tabnav::Tab.new(@template)
-#         t.links_to "/wibble"
-#         html = t.render
-#         html.should == "<li><a href=\"/wibble\"></a></li>"
-#       end
-#     end
-#
-#     it "should pass the options given on creation to the li" do
-#       @template.should_receive(:content_tag).with(:li, {:id => "my_id", :class => "my_class"}).and_return(:some_markup)
-#       t = Tabnav::Tab.new(@template, :id => "my_id", :class => "my_class")
-#       t.named "A Tab"
-#       t.render.should == :some_markup
-#     end
-#
-#     it "should pass the options given to the link to link_to" do
-#       @template.should_receive(:link_to).with("A Tab", "/wibble", {:class => "link_class", :target => "_blank"}).and_return("A Link")
-#       t = Tabnav::Tab.new(@template)
-#       t.named "A Tab"
-#       t.links_to "/wibble", :class => "link_class", :target => "_blank"
-#       html = t.render
-#       html.should == "<li>A Link</li>"
-#     end
   end
 end
